4318745dccd017cf59bfea57482a61d55e705b49
Angelina112358 <angelma112358@gmail.com>
2022-08-29 20:05:56 +0300

    Added header

---
+++ components/Header.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native'
+
+class Header extends React.Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.title}>SHIFT</Text>
+            </View>
+
+        );
+    } 
+}
+
+export default Header;
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        backgroundColor: "#FFA500",
+        elevation: 1.5,
+        marginTop: 50,
+        padding: 15
+    },
+    title: {
+        fontWeight: '700',
+        fontSize: 20,
+        color: "#fff"
+
+    }
+  });
+  
